2594dcfda2ea5219be0a09afbd0791467f16231e
B23451 <B23451@students.iitmandi.ac.in>
2026-04-09 12:23:29 +0530

    Update rombergformula.m
    Code was incomplete at the previous stage. I have completed the code and added a few aditional features.

--- rombergformula.m
+++ rombergformula.m
@@ -1,35 +1,37 @@
 clc,clearvars
 
-function [v]= evaluate(array)
-
-
-
+function []=solve(vec1,k,kg)
+vec=zeros(length(vec1)-1);
+for i=1:length(vec1)-1
+    vec(i)=(4^(k-1)*vec1(i+1)-vec1(i))/(4^(k-1)-1);
+    fprintf("%.8f  ",vec(i))
+end
+fprintf("\n")
+if(k<kg)
+    solve(vec,k+1,kg);
+end
 end
 
-f = @(x) x^2+2*x+1;
 a = input("Write the lower limit of integration: ");
 b = input("Write the upper limit of integration: ");
-k = 10; % LEVELS UPTO WHICH IT HAS TO BE EVALUATE
+k = input("Write levels upto which we have to evaluate"); % LEVELS UPTO WHICH IT HAS TO BE EVALUATE
+f = input("Write the function: ","s");
 n=2^k+1;
 value=zeros(n);
-% 
-%     2 2P
-% 4 3 2 4P
-%   3 2 8P
-%       16P
-%
+
 
 for i=1:n
-    value(i)=f(a+i*(b-a)/(n));
+    x=a+(i-1)*(b-a)/(n-1);
+    value(i)=eval(f);
 end
-trapazoidal=zeros(k,k);I=0;
+trapazoidal=zeros(k);I=0;
 for i=1:k
     I=-value(1)-value(n);
     for j=1:2^(k-i+1):n
        I=I+2*value(j);
     end
-    trapazoidal(1,i)=I*(b-a)*2^(k-i-1)/(n-1);
-    fprintf("for i = %d, I=%.4f .\n",(n-1)/2^(k-i)+1,trapazoidal(i));
+    trapazoidal(i)=I*(b-a)*2^(k-i)/(n-1);
+    fprintf("%.8f  ",trapazoidal(i))
 end
-
-
+fprintf("\n")
+solve(trapazoidal,2,k)
